ee4856ca8a4c5ec4ddac1d789ba91c9e5b4edaeb
Valter Matta <mattavalter98@gmail.com>
2024-11-21 13:57:11 +0100

    .....

--- scripts/home_page.js
+++ scripts/home_page.js
@@ -4,13 +4,12 @@ class HomePage extends Spotify {
 	#playlistsContainer;
 	#spinnerSongs;
 	#spinnerAlbums;
+	#musicElements;
 
-	constructor({
-		songsContainer,
-		playlistsContainer,
-		spinnerSongs,
-		spinnerAlbums,
-	}) {
+	constructor(
+		{ songsContainer, playlistsContainer, spinnerSongs, spinnerAlbums },
+		musicElements,
+	) {
 		super();
 
 		if (
@@ -24,6 +23,9 @@ class HomePage extends Spotify {
 		this.#playlistsContainer = playlistsContainer;
 		this.#spinnerSongs = spinnerSongs;
 		this.#spinnerAlbums = spinnerAlbums;
+		this.#musicElements = musicElements;
+
+		this.#initPlayer("rock");
 
 		this.#playlistsContainer.addEventListener("click", e => {
 			// CTA CLICK
@@ -66,6 +68,23 @@ class HomePage extends Spotify {
 							throw new Error("Add Playlist: No playlist found!");
 						createAlbum(shuffledSongs.pop());
 					}
+				});
+			};
+
+			const shuffledSongs = songs.sort(_ => Math.random() - 0.5);
+			createAlbum(shuffledSongs.pop());
+			this.#spinnerAlbums.classList.add("d-none");
+		});
+	}
+
+	#initPlayer(genre) {
+		return this.queryByGenre(genre, songs => {
+			const createAlbum = ({ album: { id } }) => {
+				this.queryBySong(id, albumSongs => {
+					new MusicPlayer(
+						this.#musicElements,
+						new Playlist(...albumSongs.map(song => new Song(song))),
+					);
 				});
 			};
 
